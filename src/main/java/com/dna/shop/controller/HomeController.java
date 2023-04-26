85f83ae3e17bb19db17051c220ce6f9faac6d63e
fantad007 <dungndm232@gmail.com>
2023-04-26 19:59:52 +0700

    Test return template with /

--- src/main/java/com/dna/shop/controller/HomeController.java
+++ src/main/java/com/dna/shop/controller/HomeController.java
@@ -14,6 +14,6 @@ public class HomeController {
     @GetMapping("/")
     public String homeView(Model model) {
         model.addAttribute("TOTAL_QUANTITIES", cartService.getTotalQuantities());
-        return "trang-chu";
+        return "/trang-chu";
     }
 }
