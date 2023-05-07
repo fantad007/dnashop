052e8598b907eb8ae0d49c205a1f667d8a6cc9b2
fantad007 <dungndm232@gmail.com>
2023-05-07 13:33:22 +0700

    Design trang-chu page

--- src/main/java/com/dna/shop/controller/HomeController.java
+++ src/main/java/com/dna/shop/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.dna.shop.controller;
 
+import com.dna.shop.service.ProductService;
 import com.dna.shop.service.ShoppingCartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,11 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     @Autowired
+    ProductService productService;
+    @Autowired
     ShoppingCartService cartService;
 
     @GetMapping(value = {"/", "/trang-chu"})
     public String homeView(Model model) {
         model.addAttribute("TOTAL_QUANTITIES", cartService.getTotalQuantities());
+        model.addAttribute("LIST_PRODUCT", productService.getAll());
         return "trang-chu";
     }
 }
