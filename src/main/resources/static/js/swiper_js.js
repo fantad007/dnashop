0a5d2ef3fb02ddee8aded52aba4d6faf02db259d
fantad007 <dungndm232@gmail.com>
2023-05-15 13:01:49 +0700

    Style card in slide and add cart thumbnail

--- src/main/resources/static/js/swiper_js.js
+++ src/main/resources/static/js/swiper_js.js
@@ -2,13 +2,18 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: "auto",
     direction: 'horizontal',
     loop: false,
-    autoplay: {
-        delay: 1000,
-        disableOnInteraction: false
-    },
+//    autoplay: {
+//        delay: 1000,
+//        disableOnInteraction: false
+//    },
     spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    }
+//    pagination: {
+//        el: ".swiper-pagination",
+//        clickable: true,
+//    }
+// Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
 });
